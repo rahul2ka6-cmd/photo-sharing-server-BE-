c4fa7bc6528e0cc178a6670327f903fc2f192f01
pikapika <rahul2ka6@gmail.com>
2026-06-03 09:35:33 +0700

    Fix dbLoad and UserRouter errors

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,31 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const User = require("../db/userModel");
+
 const router = express.Router();
 
-router.get("/list", async (request, response) => {
-  try {
-    const users = await User.find({}).select("_id first_name last_name");
-    response.status(200).json(users);
-  } catch (error) {
-    response.status(500).json({ error: error.message });
-  }
+router.post("/", async (request, response) => {
+  
 });
 
-router.get("/:id", async (request, response) => {
-  const { id } = request.params;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return response.status(400).json({ message: "ID nguoi dung khong hop le" });
-  }
-  try {
-    const user = await User.findById(id).select(
-      "_id first_name last_name location description occupation"
-    );
-    if (!user)
-      return response.status(404).json({ message: "Khong tim thay nguoi nay" });
-    response.status(200).json(user);
-  } catch (error) {
-    response.status(500).json({ message: "Loi server" });
-  }
+router.get("/", async (request, response) => {
+  
 });
 
 module.exports = router;
